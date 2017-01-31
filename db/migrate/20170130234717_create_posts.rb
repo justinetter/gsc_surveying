f805ff6a39028402931759c27c4f6d284e402b5a
ethanwilkins <ethanforrestwilkins@gmail.com>
2017-01-30 19:30:27 -0500

    post_model1

--- db/migrate/20170130234717_create_posts.rb
+++ db/migrate/20170130234717_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
+      t.integer :user_id
+      t.text :body
+      t.string :image
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122213356) do
+ActiveRecord::Schema.define(version: 20170130234717) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "body"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",    null: false
