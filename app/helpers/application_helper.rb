8767adec99c2dd549eef0375da78f0f36d027b9f
ethanwilkins <ethanforrestwilkins@gmail.com>
2017-02-04 16:34:44 -0500

    nav_menu_started1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def fa_icon icon, label='', size=''
+    str = %Q[<i class="fa fa-#{icon}#{' ' + size if size.present?}"></i>] + " " + label
+    return str.html_safe
+  end
 end
